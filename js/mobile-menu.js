cba00909b2c9d47665eb10865c61890527313bd5
dev0652 <113797010+dev0652@users.noreply.github.com>
2022-12-18 09:26:55 +0200

    Linked a js-library to disable body scroll

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,8 @@
 (() => {
-	const mobileMenu = document.querySelector('.js-menu-container');
-	const openMenuBtn = document.querySelector('.js-open-menu');
-	const closeMenuBtn = document.querySelector('.js-close-menu');
-	const mobileMenuBackdrop = document.querySelector('.js-menu-backdrop');
+	const mobileMenu = document.querySelector('[js-menu-container]');
+	const openMenuBtn = document.querySelector('[js-open-menu]');
+	const closeMenuBtn = document.querySelector('[js-close-menu]');
+	const mobileMenuBackdrop = document.querySelector('[js-menu-backdrop]');
 
 	const toggleMenu = () => {
 		const isMenuOpen =
